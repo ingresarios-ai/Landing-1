ccb81277fb7ea45c6436c2dd6603ca3200bddf1a
ingresarios-ai <ingresariostech@gmail.com>
2026-03-21 02:14:12 -0600

    feat(routing): add independent PreApp clone that boots to dashboard at /pre

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import PreApp from './PreApp.tsx'
 import RetoLanding from './RetoLanding.tsx'
 
 createRoot(document.getElementById('root')!).render(
@@ -13,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/reto" element={<RetoLanding />} />
         <Route path="/preview" element={<App />} />
         <Route path="/app" element={<App />} />
-        <Route path="/pre" element={<App />} />
+        <Route path="/pre" element={<PreApp />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
